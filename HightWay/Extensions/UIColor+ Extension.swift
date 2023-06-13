bf43fbc0d0bd8f53818bd409275e403afb5be287
Ivan Biriukov <Grottt0@mail.ru>
2023-06-14 02:47:49 +0300

    Initial commit; Create an Login Screen; Delete storyboard; Upload onboarding screen image; Upload needed fonts; Set up Extensions for Fonts and Colors;

---
+++ HightWay/Extensions/UIColor+ Extension.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+
+    static let specialOrange = UIColor(red: 235/256, green: 175/256, blue: 61/256, alpha: 1)
+    
+}
---
+++ HightWay/Extensions/UIFont+ Extension.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+extension UIFont {
+    
+    // MARK: - SemiBold
+    
+    static func specialSemiBold16() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 16)
+    }
+    
+    static func specialSemiBold18() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 18)
+    }
+    
+    static func specialSemiBold20() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 20)
+    }
+    
+    static func specialSemiBold22() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 22)
+    }
+    
+    static func specialSemiBold28() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 28)
+    }
+    
+    static func specialSemiBold30() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 30)
+    }
+    
+    static func specialSemiBold40() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 40)
+    }
+    
+    static func specialSemiBold50() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-SemiBold", size: 50)
+    }
+    
+    // MARK: - Medium
+    
+    static func specialMedium16() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-Medium", size: 16)
+    }
+    
+    static func specialMedium18() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-Medium", size: 18)
+    }
+    
+    static func specialMedium22() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-Medium", size: 22)
+    }
+    
+    static func specialMedium26() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-Medium", size: 26)
+    }
+    
+    static func specialMedium28() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-Medium", size: 28)
+    }
+    
+    // MARK: - Regular
+    
+    static func specialRegular14() -> UIFont?{
+        return UIFont(name: "MontserratAlternates-Regular", size: 14)
+    }
+}
